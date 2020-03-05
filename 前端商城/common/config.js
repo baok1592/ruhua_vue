59e64e962c3e60dbf5ec2892dd12f4a90232f178
光爵 <31500946+baok1592@users.noreply.github.com>
2020-03-05 18:43:13 +0800

    Update config.js

--- 前端商城/common/config.js
+++ 前端商城/common/config.js
@@ -1,4 +1,3 @@
-// const Api_url='/../'      
-//const Api_url='https://api.ruhuashop.com/'  
-const Api_url='http://www.tp3.com/'
+// const Api_url='/../'        
+const Api_url='http://www.xx.com/'
 export {Api_url} 
